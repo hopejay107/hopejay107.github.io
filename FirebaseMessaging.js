1f2f1df6e61ed1b6b2bc2998f288ee7884dafe67
hopejay107 <122936023+hopejay107@users.noreply.github.com>
2025-11-11 16:17:26 +0200

    Add files via upload

---
+++ FirebaseMessaging.js
@@ -0,0 +1,5 @@
+importScripts('https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging.js');
+firebase.initializeApp({
+    measurementId: "G-XXXXXXX",
+});
+const messaging = firebase.messaging();
